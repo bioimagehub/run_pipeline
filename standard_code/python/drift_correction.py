286e4300fb401b9e6ac0c8bcb7e16e11f49b590b
oodegard <oodegard@gmail.com>
2025-10-06 15:22:56 +0200

    perfect

--- standard_code/python/drift_correction.py
+++ standard_code/python/drift_correction.py
@@ -874,7 +874,7 @@ def main() -> None:
     
     # Setup logging
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.WARNING,
         format='%(asctime)s | %(levelname)s | %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
