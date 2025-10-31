73f28fde759498c69381c960e3bd9c45892652e2
bioimagehub <bioimagehub@gmail.com>
2025-10-31 15:25:27 +0100

    Update test_convert_to_tif.py

--- standard_code/python/Test_code/test_convert_to_tif.py
+++ standard_code/python/Test_code/test_convert_to_tif.py
@@ -3,8 +3,8 @@ from bioio import BioImage
 import bioio_ome_tiff, bioio_tifffile, bioio_nd2, bioio_lif, bioio_czi, bioio_dv
 
 
-path = r"F:\BIPHUB-OYVIND\test_data\different_fileformats\29102025_Apsana-04.czi"
-path_out = r"F:\BIPHUB-OYVIND\test_data\different_fileformats\29102025_Apsana-04.ome.tif"
+path = r"F:\BIPHUB-OYVIND\test_data\different_fileformats\CZI example\EW25-63c_HeLa-GFP-CETN1-mChTub_scr_si8_30hKD.czi"
+path_out = r"F:\BIPHUB-OYVIND\test_data\different_fileformats\CZI example\EW25-63c_HeLa-GFP-CETN1-mChTub_scr_si8_30hKD_test.ome.tif"
 
 
 img = BioImage(path, reader=bioio_czi.Reader)
